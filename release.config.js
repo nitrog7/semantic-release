ce7fa0d21ba660d5492607660d91dbea08654350
Giraldo Rosales <giraldo@nitrogenlabs.com>
2023-01-23 13:00:43 -0600

    fix(versioning): fix release script

--- release.config.js
+++ release.config.js
@@ -7,15 +7,14 @@ module.exports = {
     }
   ],
   plugins: [
-    'semantic-release/commit-analyzer',
-    'semantic-release/release-notes-generator',
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     [
       '@semantic-release/changelog',
       {
         changelogFile: 'CHANGELOG.md'
       }
     ],
-    '@semantic-release/npm',
     '@semantic-release/github',
     [
       '@semantic-release/git',
