049d8cae66af63a5cdd5cddd5d8117596b07c1a8
Giraldo Rosales <giraldo@nitrogenlabs.com>
2023-01-23 13:13:41 -0600

    fix(versioning): update commit config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      0
+    ],
+    'header-max-length': [
+      0,
+      'always',
+      120
+    ],
+    'body-max-line-length': [
+      0,
+      'always',
+      120
+    ]
+  }
 }
